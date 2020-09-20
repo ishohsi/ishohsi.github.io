a3e5974f6d0beb47d9ab7c1acd43c0c06122e7ed
ishohsi <70577391+ishohsi@users.noreply.github.com>
2020-09-20 09:49:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function startTime(){
 	var today=new Date();
+    var yy=today.getFullYear();
+    var mm=today.getMonth()+1;             //月份
+    var dd=today.getDate();
 	var h=today.getHours();
 	var m=today.getMinutes();
 	var s=today.getSeconds();// 在小于10的数字前加一个‘0’
     h=checkTime(h);
     m=checkTime(m);
 	s=checkTime(s);
-	document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+	document.getElementById('txt').innerHTML=yy+"."+mm+"."+dd+" "+h+":"+m+":"+s;
 	t=setTimeout(function(){startTime()},500);
 }
 function checkTime(i){
